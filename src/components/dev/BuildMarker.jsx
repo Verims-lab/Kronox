ab6652748f2e795a97968f3fc7f8b06db179ee54
Verim Sarı <sariverim@gmail.com>
2026-05-19 13:58:48 +0300

    Update BuildMarker.jsx

--- src/components/dev/BuildMarker.jsx
+++ src/components/dev/BuildMarker.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-const BUILD_MARKER = 'Codex006';
+const BUILD_MARKER = 'Codex009';
 
 export default function BuildMarker() {
   const [visible, setVisible] = useState(true);
