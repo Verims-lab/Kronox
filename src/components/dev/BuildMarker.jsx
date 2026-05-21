2c4ed13aa430e0383e76119d7567531eb82bd836
Verim Sarı <sariverim@gmail.com>
2026-05-22 00:00:44 +0300

    Increment build marker

--- src/components/dev/BuildMarker.jsx
+++ src/components/dev/BuildMarker.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-const BUILD_MARKER = 'Codex036';
+const BUILD_MARKER = 'Codex037';
 
 export default function BuildMarker() {
   const [visible, setVisible] = useState(true);
