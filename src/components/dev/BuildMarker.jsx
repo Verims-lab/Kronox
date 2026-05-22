c7788c0e5922053974524c3f0cac123cc7631991
Verim Sarı <sariverim@gmail.com>
2026-05-22 22:21:40 +0300

    Bump build marker to Codex048

--- src/components/dev/BuildMarker.jsx
+++ src/components/dev/BuildMarker.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-const BUILD_MARKER = 'Codex047';
+const BUILD_MARKER = 'Codex048';
 
 export default function BuildMarker() {
   const [visible, setVisible] = useState(true);
