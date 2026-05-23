0709f396027c5ce9ff302ed282786d1d46941d83
Verim Sarı <sariverim@gmail.com>
2026-05-24 00:54:26 +0300

    Use exact Home button image states

--- src/components/dev/BuildMarker.jsx
+++ src/components/dev/BuildMarker.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-const BUILD_MARKER = 'Codex065';
+const BUILD_MARKER = 'Codex066';
 
 export default function BuildMarker() {
   const [visible, setVisible] = useState(true);
