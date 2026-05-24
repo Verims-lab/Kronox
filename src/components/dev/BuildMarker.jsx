644d7999993d208bf4103e26ee33ce168e390fb9
Verim Sarı <sariverim@gmail.com>
2026-05-24 14:43:10 +0300

    Support multi-select online categories

--- src/components/dev/BuildMarker.jsx
+++ src/components/dev/BuildMarker.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-const BUILD_MARKER = 'Codex069';
+const BUILD_MARKER = 'Codex070';
 
 export default function BuildMarker() {
   const [visible, setVisible] = useState(true);
